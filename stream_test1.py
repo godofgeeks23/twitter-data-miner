a3d37e38f5401efa812aaaa2e3ad13c2f4ee0f8e
Aviral Srivastava <aviralji4@gmail.com>
2022-06-08 22:14:06 +0530

    Update stream_test1.py

--- stream_test1.py
+++ stream_test1.py
@@ -14,19 +14,19 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-class Linstener(tweepy.Stream):
-    tweets = []
-    limit = 1
-    def on_status(self, status):
-        self.tweets.append(status)
-        # print(status.user.screen_name + ": " + status.text)
-        if len(self.tweets) == self.limit:
-            self.disconnect()
-stream_tweet = Linstener(api_key, api_key_secret, access_token, access_token_secret)
+# class Linstener(tweepy.Stream):
+#     tweets = []
+#     limit = 1
+#     def on_status(self, status):
+#         self.tweets.append(status)
+#         # print(status.user.screen_name + ": " + status.text)
+#         if len(self.tweets) == self.limit:
+#             self.disconnect()
+# stream_tweet = Linstener(api_key, api_key_secret, access_token, access_token_secret)
 
 # stream by keywords
-keywords = ['2022', '#python']
-stream_tweet.filter(track=keywords)
+# keywords = ['2022', '#python']
+# stream_tweet.filter(track=keywords)
 
 # # stream by users
 # users = ['MehranShakarami', 'veritasium']
@@ -35,15 +35,15 @@ stream_tweet.filter(track=keywords)
 #     user_ids.append(api.get_user(screen_name=user).id)
 # stream_tweet.filter(follow=user_ids)
 
-columns = ['User', 'Tweet']
-data = []
-for tweet in stream_tweet.tweets:
-    if not tweet.truncated:
-        data.append([tweet.user.screen_name, tweet.text])
-    else:
-        data.append([tweet.user.screen_name, tweet.extended_tweet['full_text']])
-df = pd.DataFrame(data, columns=columns)
-print(df)
+# columns = ['User', 'Tweet']
+# data = []
+# for tweet in stream_tweet.tweets:
+#     if not tweet.truncated:
+#         data.append([tweet.user.screen_name, tweet.text])
+#     else:
+#         data.append([tweet.user.screen_name, tweet.extended_tweet['full_text']])
+# df = pd.DataFrame(data, columns=columns)
+# print(df)
 
 
 # keywords = '@veritasium'
@@ -55,3 +55,10 @@ print(df)
 #     data.append([tweet.user.screen_name, tweet.full_text])
 # df = pd.DataFrame(data, columns=columns)
 # print(df)
+
+# save by users
+users = ['MehranShakarami', 'veritasium']
+user_ids = []
+for user in users:
+    user_ids.append(api.get_user(screen_name=user).id)
+stream_tweet.filter(follow=user_ids)
