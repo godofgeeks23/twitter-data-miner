afa22621d673b44861ba57fc3784ccb4701c2207
Aviral Srivastava <aviralji4@gmail.com>
2022-06-08 22:14:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,17 +11,19 @@ access_token_secret = os.getenv('access_token_secret')
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth) 
+api = tweepy.API(auth)
 
 # for tweet in tweepy.Cursor(api.search, q='trump').items(10):
-    # print(tweet.text)
+# print(tweet.text)
+
 
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
         print(status.text)
 
+
 myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
 print("Starting to listen...")
 print(myStream.filter(track=['web']))
