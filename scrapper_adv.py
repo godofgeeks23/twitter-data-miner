6bdb349f78b02f81fc6afee049920bcbf0b95d2e
Aviral Srivastava <aviralji4@gmail.com>
2022-06-08 16:47:41 +0530

    Update scrapper_adv.py

--- scrapper_adv.py
+++ scrapper_adv.py
@@ -2,13 +2,22 @@ import os
 from dotenv import load_dotenv
 import tweepy
 
+load_dotenv()
+
 consumer_key = os.getenv('consumer_key')
 consumer_secret = os.getenv('consumer_secret')
 access_token = os.getenv('access_token')
 access_token_secret = os.getenv('access_token_secret')
 
-client = tweepy.Client(consumer_key= consumer_key,consumer_secret= consumer_secret,access_token= access_token,access_token_secret= access_token_secret)
-query = 'news'
-tweets = client.search_recent_tweets(query=query, max_results=10)
-for tweet in tweets.data:
-    print(tweet.text)
+# client = tweepy.Client(consumer_key= consumer_key,consumer_secret= consumer_secret,access_token= access_token,access_token_secret= access_token_secret)
+# query = 'news'
+# tweets = client.search_recent_tweets(query=query, max_results=10)
+# for tweet in tweets.data:
+#     print(tweet.text)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth) 
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+   print(tweet.text)
