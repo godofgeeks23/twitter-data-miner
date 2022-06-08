d70402192af60211fcb8fdbf1ef362c9425b3c01
Aviral Srivastava <aviralji4@gmail.com>
2022-06-08 21:05:25 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+import tweepy
+
+load_dotenv()
+
+consumer_key = os.getenv('consumer_key')
+consumer_secret = os.getenv('consumer_secret')
+access_token = os.getenv('access_token')
+access_token_secret = os.getenv('access_token_secret')
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth) 
+
+# for tweet in tweepy.Cursor(api.search, q='trump').items(10):
+    # print(tweet.text)
+
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print(status.text)
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+print("Starting to listen...")
+print(myStream.filter(track=['web']))
