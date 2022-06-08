5a7588c3f5a2d0a889cc273d2128d78ee17d1483
Aviral Srivastava <aviralji4@gmail.com>
2022-06-08 21:14:30 +0530

    Update stream_test1.py

--- stream_test1.py
+++ stream_test1.py
@@ -3,90 +3,57 @@ import pandas as pd
 from dotenv import load_dotenv
 import os
 
-# read configs
-# config = configparser.ConfigParser()
-# config.read('config.ini')
-
 load_dotenv()
 
 access_token = os.getenv('access_token')
 access_token_secret = os.getenv('access_token_secret')
-
 api_key = os.getenv('consumer_key')
 api_key_secret = os.getenv('consumer_secret')
-
-
-# authentication
 auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
 
 
-# class Linstener(tweepy.Stream):
+class Linstener(tweepy.Stream):
+    tweets = []
+    limit = 1
+    def on_status(self, status):
+        self.tweets.append(status)
+        # print(status.user.screen_name + ": " + status.text)
+        if len(self.tweets) == self.limit:
+            self.disconnect()
+stream_tweet = Linstener(api_key, api_key_secret, access_token, access_token_secret)
 
-#     tweets = []
-#     limit = 1
-
-#     def on_status(self, status):
-#         self.tweets.append(status)
-#         # print(status.user.screen_name + ": " + status.text)
-
-#         if len(self.tweets) == self.limit:
-#             self.disconnect()
-
-
-
-
-
-# stream_tweet = Linstener(api_key, api_key_secret, access_token, access_token_secret)
-
-# # stream by keywords
-# # keywords = ['2022', '#python']
-
-# # stream_tweet.filter(track=keywords)
+# stream by keywords
+keywords = ['2022', '#python']
+stream_tweet.filter(track=keywords)
 
 # # stream by users
 # users = ['MehranShakarami', 'veritasium']
 # user_ids = []
-
 # for user in users:
 #     user_ids.append(api.get_user(screen_name=user).id)
-
 # stream_tweet.filter(follow=user_ids)
 
-# # create DataFrame
-
-# columns = ['User', 'Tweet']
-# data = []
-
-# for tweet in stream_tweet.tweets:
-#     if not tweet.truncated:
-#         data.append([tweet.user.screen_name, tweet.text])
-#     else:
-#         data.append([tweet.user.screen_name, tweet.extended_tweet['full_text']])
-
-# df = pd.DataFrame(data, columns=columns)
-
-# print(df)
-
-
-
-
-keywords = '@veritasium'
-limit=300
-
-tweets = tweepy.Cursor(api.search_tweets, q=keywords, count=100, tweet_mode='extended').items(limit)
-
-# tweets = api.user_timeline(screen_name=user, count=limit, tweet_mode='extended')
-
-# create DataFrame
 columns = ['User', 'Tweet']
 data = []
-
-for tweet in tweets:
-    data.append([tweet.user.screen_name, tweet.full_text])
-
+for tweet in stream_tweet.tweets:
+    if not tweet.truncated:
+        data.append([tweet.user.screen_name, tweet.text])
+    else:
+        data.append([tweet.user.screen_name, tweet.extended_tweet['full_text']])
 df = pd.DataFrame(data, columns=columns)
-
 print(df)
+
+
+
+
+# keywords = '@veritasium'
+# limit=300
+# tweets = tweepy.Cursor(api.search_tweets, q=keywords, count=100, tweet_mode='extended').items(limit)
+# columns = ['User', 'Tweet']
+# data = []
+# for tweet in tweets:
+#     data.append([tweet.user.screen_name, tweet.full_text])
+# df = pd.DataFrame(data, columns=columns)
+# print(df)
