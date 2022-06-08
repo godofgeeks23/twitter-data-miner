646ce91cc6ada036f8e4d698da59fe0a75b3d86e
Aviral Srivastava <aviralji4@gmail.com>
2022-06-08 22:13:52 +0530

    Update scrapper_adv.py

--- scrapper_adv.py
+++ scrapper_adv.py
@@ -17,7 +17,7 @@ access_token_secret = os.getenv('access_token_secret')
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth) 
+api = tweepy.API(auth)
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
-   print(tweet.text)
+    print(tweet.text)
