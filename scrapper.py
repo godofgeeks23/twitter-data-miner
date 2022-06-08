907cc79b6e06f13c3a2f7c59e53265c733ae8b3b
Aviral Srivastava <aviralji4@gmail.com>
2022-06-08 22:13:40 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -9,7 +9,7 @@ client = tweepy.Client(os.getenv('bearer_token'))
 
 query = 'trump -is:retweet lang:en'
 tweets = tweepy.Paginator(client.search_recent_tweets, query=query,
-                              tweet_fields=['context_annotations', 'created_at'], max_results=10).flatten(limit=1)
+                          tweet_fields=['context_annotations', 'created_at'], max_results=10).flatten(limit=1)
 
 # print(tweets.keys())
 for tweet in tweets:
@@ -19,8 +19,8 @@ for tweet in tweets:
     # print(tweet.like_count)
 
     # if len(tweet.context_annotations) > 0:
-        # print(tweet.context_annotations)
-        # print()
+    # print(tweet.context_annotations)
+    # print()
 # z = [x.toJSON() for x in tweets]
 # with open('your_data.json', 'w') as out:
     # json.dump(z,out)
