35d3e9ca5d367e03f213da14d2f43099c11872f8
Aviral Srivastava <aviralji4@gmail.com>
2022-06-14 23:54:43 +0530

    Update gsheets_fetch.py

--- gsheets_fetch.py
+++ gsheets_fetch.py
@@ -4,7 +4,6 @@ import pandas as pd
 gc = pygsheets.authorize(service_file='verified-security-sources-6b7e7530d6f5.json')
 sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1ELBSsc5tQIjZFHCmTofbwflOx37CK0OOpXEiMa2Irto/edit?usp=sharing")
 
-print(sh.id)
-print(sh.worksheets())
-wk1 = sh[0] 
-print(wk1.get_all_values())
+# wk1 = sh[0]
+first_column_data = sh[0].get_col(1, include_tailing_empty=False)[1:] 
+print(first_column_data)
