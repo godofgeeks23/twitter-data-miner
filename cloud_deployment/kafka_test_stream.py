f4c7ec84ccdd2f1d68ad8b5cfe019f2070c0b77a
Aviral Srivastava <aviralji4@gmail.com>
2022-06-15 07:56:34 +0530

    Update kafka_test_stream.py

--- cloud_deployment/kafka_test_stream.py
+++ cloud_deployment/kafka_test_stream.py
@@ -5,7 +5,8 @@ import pprint
 import json
 from dotenv import load_dotenv
 import os
-import kafka 
+import kafka
+import pygsheets 
 
 load_dotenv()
 
@@ -37,9 +38,30 @@ class StreamAPI(tw.StreamingClient):
 
 producer = kafka.KafkaProducer(bootstrap_servers="localhost:9092")
 
+# streamer = StreamAPI(os.getenv('bearer_token'))
+# terms = "trump"
+# streamer.add_rules(tw.StreamRule(terms)
+# streamer.filter()
+
 streamer = StreamAPI(os.getenv('bearer_token'))
 
-terms = "trump"
+for rule in streamer.get_rules().data:
+    streamer.delete_rules(rule.id)
+print(streamer.get_rules())
 
-streamer.add_rules(tw.StreamRule(terms)
+rules = []
+# rules.append("#Darkweb OR #Criminal OR #Intelligence OR #OSINT OR #DeepWeb OR #Leaked OR #DataBreach OR #Terror OR #Drugs OR #Cryptocurrency OR #Ransomware OR #carding OR #onionlink")
+rules.append("from:godofgeeks_")
+
+gc = pygsheets.authorize(service_file='verified-security-sources-6b7e7530d6f5.json')
+sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1ELBSsc5tQIjZFHCmTofbwflOx37CK0OOpXEiMa2Irto/edit?usp=sharing")
+first_column_data = sh[0].get_col(1, include_tailing_empty=False)[1:] 
+usernames_list = [row.replace('https://twitter.com/', 'from:') for row in first_column_data]
+username_rule = " OR ".join(usernames_list)
+
+rules.append(username_rule)
+
+for rule in rules:
+    streamer.add_rules(tw.StreamRule(rule))
+    
 streamer.filter()
