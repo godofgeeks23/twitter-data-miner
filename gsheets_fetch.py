a548921cfa3799b367550afd64919d93be803119
Aviral Srivastava <aviralji4@gmail.com>
2022-06-15 07:18:00 +0530

    Update gsheets_fetch.py

--- gsheets_fetch.py
+++ gsheets_fetch.py
@@ -1,9 +1,10 @@
 import pygsheets
-import pandas as pd
+# import pandas as pd
 
 gc = pygsheets.authorize(service_file='verified-security-sources-6b7e7530d6f5.json')
 sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1ELBSsc5tQIjZFHCmTofbwflOx37CK0OOpXEiMa2Irto/edit?usp=sharing")
 
 # wk1 = sh[0]
 first_column_data = sh[0].get_col(1, include_tailing_empty=False)[1:] 
-print(first_column_data)
+usernames_list = [row.replace('https://twitter.com/', '') for row in first_column_data]
+print(usernames_list)
