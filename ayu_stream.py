9295cc56cf68ebd78f53fbc0d47846dc2150d8fa
Aviral Srivastava <aviralji4@gmail.com>
2022-06-15 22:16:43 +0530

    Update ayu_stream.py

--- ayu_stream.py
+++ ayu_stream.py
@@ -14,22 +14,26 @@ twitter_cred["CONSUMER_SECRET"] = os.getenv('consumer_secret')
 twitter_cred["ACCESS_KEY"] = os.getenv('access_token')
 twitter_cred["ACCESS_SECRET"] = os.getenv('access_token_secret')
 
-auth = tw.OAuthHandler(twitter_cred["CONSUMER_KEY"], twitter_cred["CONSUMER_SECRET"])
-auth.set_access_token(twitter_cred["ACCESS_KEY"], twitter_cred["ACCESS_SECRET"])
+auth = tw.OAuthHandler(
+    twitter_cred["CONSUMER_KEY"], twitter_cred["CONSUMER_SECRET"])
+auth.set_access_token(
+    twitter_cred["ACCESS_KEY"], twitter_cred["ACCESS_SECRET"])
 api = tw.API(auth, wait_on_rate_limit=True)
 
 # Initialize elasticsearch node
 # es = Elasticsearch("http://localhost:9200")
 
+
 class StreamAPI(tw.StreamingClient):
-    def on_data(self,raw_data):
-        #if(tweet != None):
+    def on_data(self, raw_data):
+        # if(tweet != None):
         # json_data = json.loads(raw_data)
         # with open("json_data.json","w") as f:
-            # json.dump(json_data,f)
+        # json.dump(json_data,f)
         print(raw_data)
         print()
         # es.index(index ="twitter_index",document=json_data,ignore=400)
+
 
 streamer = StreamAPI(os.getenv('bearer_token'))
 
