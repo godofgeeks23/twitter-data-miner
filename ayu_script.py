d3d255293b48a8a872249527c72bbe424bda98ca
Aviral Srivastava <aviralji4@gmail.com>
2022-06-15 22:16:30 +0530

    Update ayu_script.py

--- ayu_script.py
+++ ayu_script.py
@@ -13,8 +13,10 @@ twitter_cred["CONSUMER_SECRET"] = os.getenv('consumer_secret')
 twitter_cred["ACCESS_KEY"] = os.getenv('access_token')
 twitter_cred["ACCESS_SECRET"] = os.getenv('access_token_secret')
 
-auth = tw.OAuthHandler(twitter_cred["CONSUMER_KEY"], twitter_cred["CONSUMER_SECRET"])
-auth.set_access_token(twitter_cred["ACCESS_KEY"], twitter_cred["ACCESS_SECRET"])
+auth = tw.OAuthHandler(
+    twitter_cred["CONSUMER_KEY"], twitter_cred["CONSUMER_SECRET"])
+auth.set_access_token(
+    twitter_cred["ACCESS_KEY"], twitter_cred["ACCESS_SECRET"])
 api = tw.API(auth, wait_on_rate_limit=True)
 # Initialize elasticsearch node
 es = Elasticsearch("http://localhost:9200")
@@ -23,24 +25,25 @@ public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
 
+
 def acqData(search, acq):
     # Create an index name using our search criteria and today's date
     index_name = search.split(' ')[0] + '-' + dt.today().strftime('%Y-%m-%d')
     # Initialize the feed list of dictionaries
     feed = []
-    
+
     print('::Acquiring Data::')
-   
+
     # Data Acquisition
     for tweet in tw.Cursor(api.search_tweets, q=search, tweet_mode='extended').items(acq):
         feed.append(tweet._json)
-    with open("json_dat.json","w") as f:
-        json.dump(feed,f) 
+    with open("json_dat.json", "w") as f:
+        json.dump(feed, f)
 # Formatting the data and extracting what we need
     count = 0
-    
+
     print('::Transferring to Elasticsearch Search::')
-    
+
     while count < len(feed):
         # Created variables instead of directly injecting it to the dictionary because it's easier to read
         tweet_date = feed[count]['created_at']
@@ -76,6 +79,7 @@ def acqData(search, acq):
         }
         # Import into elasticsearch using the generated index name at the top of the function <search> + <date>
         es.index(index=index_name, document=doc)
-        count +=1
+        count += 1
+
 
 acqData('covid', 100)
